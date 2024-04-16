c9b06376c15cc5628f8dbacbc56c85637b27b477
sbm_deni <sbm_deni@SBMB-NB-IT0394>
2024-04-16 10:16:44 +0700

    Add project files.

---
+++ PDFjsMvc/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MvcApplication25.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.filePath = "https://getsamplefiles.com/download/pdf/sample-1.pdf";
+            return View();
+        }
+    }
+}
